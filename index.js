60a4919647355e21b79171979770681096dd2c80
Abinash1000 <113450858+Abinash1000@users.noreply.github.com>
2024-08-24 18:10:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectDb = require("./utiles/db");
 const cors = require('cors');
 
 const corsOptions = {
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5175",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credential : true,
 };
